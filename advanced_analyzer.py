8a8a242f44aa4b516c9f7d8b243c6e54bed005ea
BalochistanAi92 <fnaz1329@gmail.com>
2025-11-14 00:55:00 +0500

    Add files via upload
    "Added advanced text analyzer project"

---
+++ advanced_analyzer.py
@@ -0,0 +1,12 @@
+print("=== ADVANCED TEXT ANALYZER ===")
+
+my_text = "I am becoming an AI expert from Balochistan. I will get a scholarship to china. This is just the beginning of my journey."
+words = my_text.split()
+total_words = len(words)
+
+print("🔍ANALYSIS RESULTS:")
+print("Text:", my_text)
+print("Total words:", total_words)
+print("Word list:", words)
+print("First word:", words[0])
+print("Last word:", words[-1]) 
